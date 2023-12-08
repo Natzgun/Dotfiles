161b9e5b15d9fd51f18035b91bed7da84762ce47
Natzgun <itzlilouxx@gmail.com>
2023-12-08 16:30:40 -0500

    Readme updated in mode ORG

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -207,8 +207,8 @@
  org-download-method 'directory
  org-download-image-dir "./images"
  org-download-heading-lvl nil
- org-download-timestamp "%Y%m%d-%H%M%S_"
- org-image-actual-width 400)
+ org-download-timestamp "%Y%m%d-%H%M%S_")
+;; org-image-actual-width 400)
 
 ;; (after! org
 ;;   (setq org-download-method 'directory)
