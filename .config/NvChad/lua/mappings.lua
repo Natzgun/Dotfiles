05a241789ed918038a45b096eb75c20b6c81cd12
Natzgun <itzlilouxx@gmail.com>
2026-02-25 22:34:46 -0500

    add: keymaps for use cmake

--- .config/NvChad/lua/mappings.lua
+++ .config/NvChad/lua/mappings.lua
@@ -8,3 +8,15 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Grupo CMake: Prefijo <leader> + k
+
+map("n", "<leader>kb", "<cmd>CMakeBuild<CR>", { desc = " CMake Build" })
+map("n", "<leader>kr", "<cmd>CMakeRun<CR>", { desc = " CMake Run" })
+map("n", "<leader>kc", "<cmd>CMakeClean<CR>", { desc = " CMake Clean" })
+map("n", "<leader>kh", "<cmd>CMakeCloseRunner<CR>", { desc = "CM Hide Runner" })
+map("n", "<leader>ko", "<cmd>CMakeOpenRunner<CR>", { desc = "CM Open Runner" })
+map("n", "<leader>ks", "<cmd>CMakeStopRunner<CR>", { desc = "CM Stop Runner" })
+
+--  Terminal mode  --
+map("t", "jk", "<C-\\><C-n>", { desc = "Terminal Normal Mode" })
